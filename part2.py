6b2ff518190bf87a20e25f6388120f7d29393de4
sindrelothe <sindrl@online.no>
2023-10-25 22:15:04 +0200

    refactor to easier to use db access

--- part2.py
+++ part2.py
@@ -2,6 +2,11 @@ from DataParser import DataParser
 from DbConnector import DbConnector
 from DBWriter import DBWriter
 
+
+USER="User"
+ACTIVITY="Activity"
+TRACKPOINT="TrackPoint"
+
 class Task2:
     def __init__(self):
         self.connection = DbConnector(DATABASE="my_db", HOST="localhost", USER="test_user", PASSWORD="test_password")
@@ -11,15 +16,15 @@ class Task2:
         self.parser = DataParser("sdd-assignment3/dataset")
 
     def task1(self):
-        users_count= self.db.User.count_documents({})
+        users_count= self.db[USER].count_documents({})
         print(f"User count: {users_count}")
-        activity_count = self.db.Activity.count_documents({})
+        activity_count = self.db[ACTIVITY].count_documents({})
         print(f"Activity count: {activity_count}")
-        track_point_count=self.db.TrackPoint.count_documents({})
+        track_point_count=self.db[TRACKPOINT].count_documents({})
         print(f"Trackpoint count: {track_point_count}")
 
     def task2(self):
-        user_list=self.db.User.find({})
+        user_list=self.db[USER].find({})
         activity_counts = []
         for user in user_list:
             activity_counts.append(len(user["activities"]))
@@ -29,18 +34,18 @@ class Task2:
 
 
     def task3(self):
-        user_list=self.db.User.find({})
+        user_list=self.db[USER].find({})
         users_and_count = [(user["_id"], len(user["activities"])) for user in user_list]
         users_and_count.sort(key=lambda x : x[1], reverse=True)
         twenty_users_with_most_activities = [uac[0] for uac in users_and_count[:20]]
         print(twenty_users_with_most_activities)
 
     def task4(self):
-        user_list=self.db.User.find({})
+        user_list=self.db[USER].find({})
         taxi_users = []
         for user in user_list:
             for activity_id in user["activities"]:
-                user_activity = self.db.Activity.find_one({"_id": activity_id})
+                user_activity = self.db[ACTIVITY].find_one({"_id": activity_id})
                 if "taxi" ==user_activity["type"]:
                     taxi_users.append(user["_id"])
                     break
@@ -53,4 +58,4 @@ if __name__ == "__main__":
     # task.task1()
     # task.task2()
     # task.task3()
-    task.task4()
+    # task.task4()
